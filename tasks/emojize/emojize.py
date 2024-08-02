eea7a632f5b0d263a8ad8aa3a33735d9ce4f130e
sahin <80550947+sahinyes@users.noreply.github.com>
2024-08-02 09:40:58 +0200

    Update emojize.py

--- tasks/emojize/emojize.py
+++ tasks/emojize/emojize.py
@@ -1,4 +1,6 @@
-import emoji
-a = str(input("Input:"))
-b = emoji.emojize(a)
-print("Output:",b)
+from emoji import emojize
+user_input = input("Enter your text: ")
+print("Output:", emojize(user_input, language="alias"))
+
+# edited again according to the suggestion/correction here
+# https://github.com/sahinyes/CS50_Python/issues/2
